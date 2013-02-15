b32a51b660d382643055ca5d602bc0bfc626db8f
lirongyuan <lirongyuan1000@gmail.com>
2013-02-15 00:25:10 -0500

    Created a rails application, added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellocsgod::Application.config.secret_token = '8a175071eb0072efbb6582fb5ff4802e05fdaa47bf943d37c2914754c4b31f477d836e90774169335dfa91912d520392edf23daf89c644bc847b3fde4204e39b'
